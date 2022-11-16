3c3123d8baae052c2492a1ae7b08aa541124d9d8
Elena Reyes <estefyreyes.92@gmail.com>
2022-11-16 09:26:18 -0500

    first step: create 3 white square shapes one next to each other

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,1 +1,28 @@
 
+from turtle import Screen, Turtle
+
+#create 3 squares one next to each other
+for cuadrado in range(3):
+    new_cuadrado = Turtle(shape="square")
+    new_cuadrado.color("white")
+    new_cuadrado.pensize(20)
+    x = cuadrado * (-20)
+    new_cuadrado.goto(x, 0)
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("La viborita")
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
