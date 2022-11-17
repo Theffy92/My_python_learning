b0db551108512ce323a4c6444f768c50afe1d4d0
Elena Reyes <estefyreyes.92@gmail.com>
2022-11-17 11:51:13 -0500

    create new class called Snake

---
+++ snake_game/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+    def create_snake(self):
+        # create 3 squares one next to each other
+        for position in STARTING_POSITION:
+            new_cuadrado = Turtle("square")
+            new_cuadrado.color("white")
+            new_cuadrado.penup()
+            new_cuadrado.goto(position)
+            self.segments.append(new_cuadrado)
+
+    def move(self):
+        # for loop for the snake to move forward without every square moves apart from the other ones.
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
