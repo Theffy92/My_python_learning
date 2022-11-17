87572d09ee13da0610eab7e8143738a189f3bdf5
Elena Reyes <estefyreyes.92@gmail.com>
2022-11-17 11:52:09 -0500

    modify some lines of code to work with a new module called Snake

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,18 +1,31 @@
 
 from turtle import Screen, Turtle
-
-#create 3 squares one next to each other
-for cuadrado in range(3):
-    new_cuadrado = Turtle(shape="square")
-    new_cuadrado.color("white")
-    new_cuadrado.pensize(20)
-    x = cuadrado * (-20)
-    new_cuadrado.goto(x, 0)
+import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("La viborita")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+
 
 
 
